54ad5ca02d53ae8e66e4aade4a113598b2baea4b
Avni Aggarwal <137143343+Avniagg27@users.noreply.github.com>
2023-11-30 16:27:40 +0530

    Create exprtreenode.cpp

---
+++ exprtreenode.cpp
@@ -0,0 +1,26 @@
+/* Do NOT add/remove any includes statements from this header file */
+/* unless EXPLICTLY clarified on Piazza. */
+#include "exprtreenode.h"
+
+//Write your code below this line
+
+ExprTreeNode::ExprTreeNode(){
+    type = "";
+    id = "";
+    num = 0;
+    left = NULL;
+    right = NULL;
+}
+
+ExprTreeNode::ExprTreeNode(string t,int v){
+    type = t;
+    num =  v;
+    id = "";
+    left = NULL;
+    right = NULL;
+}
+
+ExprTreeNode::~ExprTreeNode(){
+    left = NULL;
+    right = NULL;  
+}
